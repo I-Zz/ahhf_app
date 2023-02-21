78108ffa50ec275c552a8b5009be81bc2e767ed6
I-Zz <21bds022@iiitdwd.ac.in>
2023-02-21 23:01:26 +0530

    modified Bottom Navigation Bar in Tabs Screen

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,8 +2,6 @@ import 'package:ahhf_app/screens/home.dart';
 import 'package:ahhf_app/screens/profile_screen.dart';
 import 'package:ahhf_app/screens/projects_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
 
 class TabsScreen extends StatefulWidget {
   static const String id = 'TabsPage';
@@ -41,46 +39,138 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: _pages[_selectedPageIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: _selectPage,
-        backgroundColor: Color.fromRGBO(255, 255, 255, 1),
-        currentIndex: _selectedPageIndex,
-        
-        items: [
-          BottomNavigationBarItem(
-            icon: Row(
-              children: <Widget>[
-                const Spacer(),
-                const Spacer(),
-                Icon(Icons.home_outlined),
-                Text('Home'),
-                const Spacer(),
-              ],
+      bottomNavigationBar: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 21,
+          vertical: 0,
+        ),
+        // margin: EdgeInsets.all(0),
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(255, 255, 255, 1),
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 6,
+              spreadRadius: 0,
+              offset: Offset(0, -7),
+              color: Color.fromRGBO(0, 0, 0, 0.06),
             ),
-            label: '',
+          ],
+        ),
+        child: BottomNavigationBar(
+          unselectedFontSize: 0,
+          selectedFontSize: 0,
+          onTap: _selectPage,
+          backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+          currentIndex: _selectedPageIndex,
+          elevation: 0,
+          items: [
+            BottomNavigationBarItem(
+              activeIcon: ActiveIcon(
+                iconData: Icons.home_outlined,
+                title: 'Home',
+              ),
+              icon: InactiveIcon(
+                iconData: Icons.home_outlined,
+                title: 'Home',
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: ActiveIcon(
+                iconData: Icons.dashboard_outlined,
+                title: 'Projects',
+              ),
+              icon: InactiveIcon(
+                iconData: Icons.dashboard_outlined,
+                title: 'Projects',
+              ),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              activeIcon: ActiveIcon(
+                iconData: Icons.account_circle_outlined,
+                title: 'Profile',
+              ),
+              icon: InactiveIcon(
+                iconData: Icons.account_circle_outlined,
+                title: 'Profile',
+              ),
+              label: '',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class InactiveIcon extends StatelessWidget {
+  IconData iconData;
+  String title;
+  InactiveIcon({required this.iconData, required this.title});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(0),
+      height: 38,
+      decoration: const BoxDecoration(
+          // color: Colors.blue,
           ),
-          BottomNavigationBarItem(
-            icon: Row(
-              children: <Widget>[
-                const Spacer(),
-                Icon(Icons.dashboard_outlined),
-                Text('Projects'),
-                const Spacer(),
-              ],
-            ),
-            label: '',
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            iconData,
+            color: Color.fromRGBO(66, 143, 212, 1),
+            size: 20,
           ),
-          BottomNavigationBarItem(
-            icon: Row(
-              children: <Widget>[
-                const Spacer(),
-                Icon(Icons.account_circle_outlined),
-                Text('Profile'),
-                const Spacer(),
-                const Spacer(),
-              ],
+          const SizedBox(width: 7),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Color.fromRGBO(66, 143, 212, 1),
+              fontSize: 12,
+              fontFamily: 'Montserrat',
+              // fontWeight: FontWeight.w600,
             ),
-            label: '',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ActiveIcon extends StatelessWidget {
+  IconData iconData;
+  String title;
+  ActiveIcon({required this.iconData, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(0),
+      height: 38,
+      decoration: BoxDecoration(
+        color: const Color.fromRGBO(66, 143, 212, 1),
+        borderRadius: BorderRadius.circular(45),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            iconData,
+            color: Colors.white,
+            size: 20,
+          ),
+          const SizedBox(width: 7),
+          Text(
+            title,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 12,
+              fontFamily: 'Montserrat',
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ],
       ),
